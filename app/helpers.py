96f9336bb5830fa9cc687000bcdea372e6e6fbc7
Jeroen Schaftenaar <4943036+jschaftenaar@users.noreply.github.com>
2024-09-29 16:00:43 -0500

    Add json file support, make discord optional

--- app/helpers.py
+++ app/helpers.py
@@ -1,6 +1,12 @@
 import yaml
+from werkzeug.routing import BaseConverter
 
 def read_yaml_file(yaml_file):
     with open(yaml_file, 'r', encoding='utf-8') as file:
         yaml_file_contents = yaml.safe_load(file)
     return yaml_file_contents
+
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super().__init__(url_map)
+        self.regex = items[0]
--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template, send_from_directory, request
+from flask import Flask, render_template, send_from_directory, request, jsonify
 from youtube_api import get_channel_videos, get_channel_playlists, get_channel_info, get_video_details, get_playlist_details, get_playlist_videos
 import os
 from dotenv import load_dotenv
-from helpers import read_yaml_file
+from helpers import read_yaml_file, RegexConverter
 import markdown
-import json
 
 load_dotenv()
 app = Flask(__name__)
+app.url_map.converters['regex'] = RegexConverter
 CHANNEL_ID=os.getenv('YOUTUBE_CHANNEL_ID')
 script_dir = os.path.dirname(os.path.realpath(__file__))
 configuration = read_yaml_file(os.path.join(f'{script_dir}', os.getenv('CONFIGURATION_FILE','configuration.yaml')))
@@ -26,7 +26,10 @@ def inject_videos_and_info():
 
 @app.route('/')
 def index():
-    return render_template('videos.html')
+    if video_id := configuration.get('lander', {}).get('video_id'):
+        video_details = get_video_details(video_id)
+        return render_template('index.html', video=video_details)
+    return render_template('index.html')    
 
 @app.route('/favicon.ico')
 def favicon():
@@ -42,33 +45,49 @@ def content(markdown_file):
         return render_template('content.html', content=html_content)
     return render_template('404.html'), 404
 
+@app.route('/video/<video_id>.json')
 @app.route('/video/<video_id>')
 def video(video_id):
     video_details = get_video_details(video_id)
+    if request.content_type == 'application/json' or request.path.endswith('json'):
+        return jsonify(video_details)
     return render_template('video.html', video=video_details)
 
+@app.route('/playlist/<playlist_id>.json')
 @app.route('/playlist/<playlist_id>')
 def playlist(playlist_id):
     playlist_details = get_playlist_details(playlist_id)
     playlist_videos = get_playlist_videos(playlist_id)
+    if request.content_type == 'application/json' or request.path.endswith('json'):
+        return jsonify({
+            'playlist':playlist_details,
+            'playlist_videos':playlist_videos
+        })
     return render_template('playlist.html', playlist=playlist_details, playlist_videos=playlist_videos)
 
+@app.route('/playlist/<playlist_id>/<video_id>.json')
 @app.route('/playlist/<playlist_id>/<video_id>')
 def playlist_video(playlist_id, video_id):
     playlist_details = get_playlist_details(playlist_id)
     video_details = get_video_details(video_id)
+    if request.content_type == 'application/json' or request.path.endswith('json'):
+        return jsonify({
+            'playlist': playlist_details,
+            'video': video_details
+        })
     return render_template('playlistvideo.html', playlist=playlist_details, video=video_details)
 
 @app.errorhandler(404)
 def page_not_found(_error):
     return render_template('404.html'), 404 
 
-@app.route('/videos')
-@app.route('/playlists')
-@app.route('/about')
-@app.route('/discord')
-def template():
-    return render_template(f'{request.path.strip("/")}.html')
+@app.route('/<regex("(videos|playlists|about|discord)"):slug>')
+def template(slug):
+    return render_template(f'{slug}.html')
+
+@app.route('/<regex("(videos|playlists|channel|configuration)"):slug>.json')
+def json_context(slug):
+    return jsonify(inject_videos_and_info()[slug])
 
 
 if __name__ == "__main__":
